9bda64d02522466a252df19e77dc1197ff563767
DavidCs9 <davidcastrosi@hotmail.com>
2024-07-25 12:58:32 -0600

    chore: Update page.tsx to add authentication and conditional rendering for expenses table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@
 //   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 // );
 
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export default async function HomePage() {
@@ -15,25 +16,36 @@ export default async function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1 className="text-4xl font-bold">Expenses</h1>
-      <table className="w-full table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Date</th>
-            <th className="px-4 py-2">Description</th>
-            <th className="px-4 py-2">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {[...expenses, ...expenses, ...expenses].map((expense) => (
-            <tr key={expense.id} className="text-center">
-              <td className="px-4 py-2">{expense.expenseDate}</td>
-              <td className="px-4 py-2">{expense.description}</td>
-              <td className="px-4 py-2">{expense.amount} $</td>
+      <SignedOut>
+        <div className="text-gray-400">
+          Pleased{" "}
+          <label className="text-white">
+            <SignInButton />
+          </label>{" "}
+          to see your expenses
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <h1 className="text-4xl font-bold">Expenses</h1>
+        <table className="w-full table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Date</th>
+              <th className="px-4 py-2">Description</th>
+              <th className="px-4 py-2">Amount</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {[...expenses, ...expenses, ...expenses].map((expense) => (
+              <tr key={expense.id} className="text-center">
+                <td className="px-4 py-2">{expense.expenseDate}</td>
+                <td className="px-4 py-2">{expense.description}</td>
+                <td className="px-4 py-2">{expense.amount} $</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </SignedIn>
     </main>
   );
 }
